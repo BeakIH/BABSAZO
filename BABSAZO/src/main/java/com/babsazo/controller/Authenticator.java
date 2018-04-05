eaac0cad72560b59ed000a5513742b262aa36462
BeakIH <msh3207@naver.com>
2018-04-05 10:38:51 +0900

    init

---
+++ BABSAZO/src/main/java/com/babsazo/controller/Authenticator.java
@@ -0,0 +1,5 @@
+package com.babsazo.controller;
+
+public interface Authenticator {
+	void authenticate(String id, String password);
+}
---
+++ BABSAZO/src/main/java/com/babsazo/model/BabsazoDTO.java
@@ -0,0 +1,55 @@
+package com.babsazo.model;
+
+public class BabsazoDTO {
+
+	private String babnm;
+
+	private String nickname;
+
+	private String babrole;
+
+	private String comments;
+
+	private String img;
+
+	public String getBabnm() {
+		return babnm;
+	}
+
+	public void setBabnm(String babnm) {
+		this.babnm = babnm;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getBabrole() {
+		return babrole;
+	}
+
+	public void setBabrole(String babrole) {
+		this.babrole = babrole;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+}
