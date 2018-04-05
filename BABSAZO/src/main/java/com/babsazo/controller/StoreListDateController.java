53cc5130378ff21f5e4866858bb5710be2cfe7fa
user1 <user1@DESKTOP-BIBKLL9>
2018-04-05 17:48:42 +0900

    도준희 04-05  오후 5시 50분

---
+++ BABSAZO/src/main/java/com/babsazo/controller/StoreListDateController.java
@@ -0,0 +1,79 @@
+package com.babsazo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.babsazo.model.MenuDataCommand;
+import com.babsazo.model.StoreListDateCommand;
+import com.babsazo.service.StoreListDateService;
+
+
+
+@Controller
+public class StoreListDateController {
+		
+	@Autowired
+	private StoreListDateService storeListDateService;
+	
+	
+	/*@ModelAttribute
+	public StoreListDateCommand reserveDateCommand() {
+		return new StoreListDateCommand();
+	}*/
+	
+	
+	@RequestMapping("/main/list/list_info.do")
+	public ModelAndView listInfoPage(@RequestParam int store_no){
+		
+	
+		
+		System.out.println("store_no=" + store_no);
+		
+		ModelAndView mav = new ModelAndView();
+		//store_list 가져오기
+		List<StoreListDateCommand> storeListInfo = new ArrayList<StoreListDateCommand>();
+		storeListInfo.add(storeListDateService.selectOneStoreListDate(store_no).get(0));
+		
+		
+		//preorder 가져오기
+		List<MenuDataCommand> menuList = storeListDateService.selectFoodMenu(store_no);
+		
+		mav.setViewName("main/list/list_info");
+		mav.addObject("menuList" , menuList);
+		mav.addObject("storeListInfo", storeListInfo);
+		return mav;
+
+	}
+	
+	@RequestMapping("/main/list/reserve.do")
+	public ModelAndView reservePage(@RequestParam int store_no){
+		ModelAndView mav = new ModelAndView();
+		
+		//store_list 가져오기
+		List<StoreListDateCommand> storeListInfo = new ArrayList<StoreListDateCommand>();
+		storeListInfo.add(storeListDateService.selectOneStoreListDate(store_no).get(0));
+		
+		//preorder 가져오기
+		List<MenuDataCommand> menuList = storeListDateService.selectFoodMenu(store_no);
+		
+		/*//member 가져오기
+		List<MemSelectDateCommand> memList = new ArrayList<MemSelectDateCommand>();
+		List<MemSelectDateCommand> MemList = storeListDateService.selectMember(mem_no);
+		memList.add(storeListDateService.selectMember(mem_no).get(0));*/
+		mav.setViewName("main/list/reserve");
+		mav.addObject("menuList" , menuList);
+		mav.addObject("storeListInfo", storeListInfo);
+		/*mav.addObject("memList", memList);*/
+		return mav;
+	}
+		
+	
+}
---
+++ BABSAZO/src/main/java/com/babsazo/service/StoreListDateService.java
@@ -0,0 +1,39 @@
+package com.babsazo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.babsazo.dao.StoreListDateDao;
+import com.babsazo.model.MemSelectDateCommand;
+import com.babsazo.model.MenuDataCommand;
+import com.babsazo.model.StoreListDateCommand;
+
+
+
+
+@Component
+public class StoreListDateService  {
+	
+	
+	@Autowired
+	private StoreListDateDao storeListDateDao;
+	
+	
+	
+	public List<StoreListDateCommand> selectOneStoreListDate(int store_no) {
+		System.out.println("selectOneStoreListDate() 확인");
+		return storeListDateDao.getStoreListData("storeList.selectOneStoreListDate" , store_no);
+	}
+	
+	public List<MenuDataCommand> selectFoodMenu(int store_no){
+		System.out.println("selectFoodMenu() 확인");
+		return storeListDateDao.getStoreListData("storeList.selectFoodMenu", store_no);
+	}
+	
+	public List<MemSelectDateCommand> selectMember(int mem_no){
+		return storeListDateDao.getStoreListData("storeList.selectMember", mem_no);
+	}
+	
+}
